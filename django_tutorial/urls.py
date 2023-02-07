0b506b79e04d02f0b959858997fb8d4aaaac57f9
Eyjólfur Ingi Ásgeirsson <eyjo@VFD-M19-10651.local>
2023-02-07 15:49:51 +0000

    fixing index webpage

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from polls.views import IndexView
+
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
